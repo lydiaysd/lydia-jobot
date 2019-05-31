30ce7ca58ec023f09afeb075b59506f12854ceb6
Bart Karnath <karnath.bart@gmail.com>
2019-05-31 18:14:35 +0100

    started with an update form on show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update]
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user = User.update(set_params)
+    redirect_to jobs_path
+  end
+
+  private
+
+  def find_user
     @user = User.find(params[:id])
+  end
+
+  def set_params
+    params.require(:user).permit(
+      :name, :job_title, :location, :total_compensation,
+      :years_experience, :education_degree, :visa_sponsor,
+      :date_posted, :employment_type
+    )
   end
 end
